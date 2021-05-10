9c9a859374bf97fa34cf98cce91aeafb6fca358f
AlexFong <361542446@qq.com>
2021-05-10 23:27:39 +0800

    初始化项目工程

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <h1>这是home</h1>
+    <el-button>123</el-button>
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  // components: {
+  //   HelloWorld,
+  // },
+};
+</script>
